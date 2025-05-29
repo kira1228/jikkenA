79fa8612d750a5bfaef131b257dbd39aa2688f2a
吉原輝人 <kiratoyoshihara@gmail.com>
2025-05-29 17:40:43 +0900

    update route

--- navigate_waypoints.py
+++ navigate_waypoints.py
@@ -4,7 +4,7 @@ import math
 from geometry_msgs.msg import PoseStamped
 from nav2_simple_commander.robot_navigator import BasicNavigator, TaskResult
 
-def make_pose(x: float, y: float, yaw: float) -> PoseStamped:
+def make_pose(x: float, y: float, z: float, w:float) -> PoseStamped:
     pose = PoseStamped()
     pose.header.frame_id = 'map'
     pose.header.stamp = rclpy.clock.Clock().now().to_msg()
@@ -14,8 +14,8 @@ def make_pose(x: float, y: float, yaw: float) -> PoseStamped:
     pose.pose.position.z = 0.0
     pose.pose.orientation.x = 0.0
     pose.pose.orientation.y = 0.0
-    pose.pose.orientation.z = 0.0
-    pose.pose.orientation.w = 0.0
+    pose.pose.orientation.z = z
+    pose.pose.orientation.w = w
     return pose
 
 def main():
@@ -27,11 +27,13 @@ def main():
     init = make_pose(0.0, 0.0, 0.0)
     navigator.setInitialPose(init)
 
-    # list of waypoints : [x, y, yaw]
+    # list of waypoints: [x, y, z, w]
     route = [
-        (1.0, 0.5, 0.0),
-        (2.0, -0.3, 0.0),
-        (0.5, -1.5, 0.0),
+        (-0.178, -0.600, -0.116, 0.993),
+        (2.178, -0.865, -0.987, 0.156),
+        (1.083, 1.165, -0.117, 0,993),
+        (3.028, 1.096, 0.902, 0.431),
+        (1.829, 2.108, 0.970, 0.239)
     ]
     poses = [make_pose(x, y, yaw) for x, y, yaw in route]
 
